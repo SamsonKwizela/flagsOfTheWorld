fc83101601d00ea7d14606f3d4596a9965935dd4
Samson Kwizela <kwizelasamson@gmail.com>
2025-01-06 22:44:22 +0200

    modified the function

--- script/componets/CountryDetails.js
+++ script/componets/CountryDetails.js
@@ -2,31 +2,50 @@ import React, { useState, useEffect } from "react";
 
 const CountryDetails = () => {
   const [details, setDetails] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true); 
   const [error, setError] = useState(null);
 
-useEffect( ()=>{
+  useEffect(() => {
+    const fetchCountryData = async () => {
+      setLoading(true); 
+      try {
+        const response = await fetch("https://restcountries.com/v3.1/all");
+        if (!response.ok) {
+          throw new Error("Failed to fetch data.");
+        }
+        const data = await response.json();
+        setDetails(data); 
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false); 
+      }
+    };
 
-  const fetchCountryData = async() => {
-    try{
-    const responce = await fetch('https://restcountries.com/v3.1/all')
-    const data = await responce.json();
-    console.log(data);
-    } catch(error){
-    setError(error)
-    } finally{
-      setLoading(false);
-    }
-}
+    fetchCountryData();
+  }, []); 
 
-  },[])
-}
-if(loading){
-  return <div>Loading.... please wait!</div>
-}
-if(error){
-  return <p>Ooops!! sorry, something went wrong</p>
-}
+  if (loading) {
+    return <div>Loading... please wait!</div>;
+  }
 
+  if (error) {
+    return <p>Oops! Sorry, something went wrong: {error}</p>;
+  }
+
+  return (
+    <div>
+      <h1>Country Details</h1>
+      <ul>
+        {details &&
+          details.map((country) => (
+            <li key={country.cca3}>
+              {country.name.common} - Population: {country.population}
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
 
 export default CountryDetails;
