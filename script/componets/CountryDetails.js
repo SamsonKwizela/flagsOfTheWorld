0503c402dad5a7067a6b97b391620cf281a7ba66
Samson Kwizela <kwizelasamson@gmail.com>
2025-01-06 22:28:05 +0200

    modified the function

--- script/componets/CountryDetails.js
+++ script/componets/CountryDetails.js
@@ -11,14 +11,21 @@ useEffect( ()=>{
     try{
     const responce = await fetch('https://restcountries.com/v3.1/all')
     const Data = await responce.json();
-    }catch{
-
+    } catch(error){
+    setError(error)
+    } finally{
+      setLoading(false);
     }
 }
 
-  }
-)
-
+  },[])
 }
+if(loading){
+  return <div>Loading.... please wait!</div>
+}
+if(error){
+  return <p>Ooops!! sorry, something went wrong</p>
+}
+
 
 export default CountryDetails;
